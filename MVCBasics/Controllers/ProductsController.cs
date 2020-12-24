f78ae5b5630e1f16d3c27635df2ee3e616d0ecfa
Nouman Baloch <nomibaloch775@hotmail.com>
2020-12-24 21:59:00 +0500

    Basic MVC APP

---
+++ MVCBasics/Controllers/ProductsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCBasics.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public string Index()
+        {
+            return "I am on Product Page";
+        }
+    }
+}
