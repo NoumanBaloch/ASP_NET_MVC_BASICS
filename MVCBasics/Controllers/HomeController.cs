7010e4300d667b84d3989fb5c434326899923a01
Nouman Baloch <nomibaloch775@hotmail.com>
2020-12-31 22:59:28 +0500

    Add Customer with In Memory cache

--- MVCBasics/Controllers/HomeController.cs
+++ MVCBasics/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Caching;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,6 +10,22 @@ namespace MVCBasics.Controllers
 {
     public class HomeController : Controller
     {
+        ObjectCache cache = MemoryCache.Default;
+        List<Customer> customers;
+
+        public HomeController()
+        {
+            customers = cache["customers"] as List<Customer>;
+            if(customers == null)
+            {
+                customers = new List<Customer>();
+            }
+        }
+
+        public void SaveCache()
+        {
+            cache["customers"] = customers;
+        }
         public ActionResult Index()
         {
             return View();
@@ -37,19 +54,22 @@ namespace MVCBasics.Controllers
             customer.PhoneNumber = postedCustomer.PhoneNumber;
             return View(customer);
         }
-
+        
         public ActionResult AddCustomer()
         {
             return View();
         }
 
+        [HttpPost]
+        public ActionResult AddCustomer(Customer customer)
+        {
+            customer.Id = Guid.NewGuid().ToString();
+            customers.Add(customer);
+            SaveCache();
+            return RedirectToAction("CustomerList");
+        }
         public ActionResult CustomerList()
         {
-            List<Customer> customers = new List<Customer>();
-
-            customers.Add(new Customer() { Name = "Nouman", PhoneNumber = "123" });
-            customers.Add(new Customer() { Name = "Kashif", PhoneNumber = "456" });
-
             return View(customers);
 
         }
